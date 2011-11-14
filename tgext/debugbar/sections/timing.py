5782c5bb74a653b7f8e6bf3802a2d8ceda07ca95
Alessandro Molina <alessandro.molina@axant.it>
2011-11-14 15:24:03 +0100

    use cProfile and avoid using a common profiler, this should report more precise profiling

--- tgext/debugbar/sections/timing.py
+++ tgext/debugbar/sections/timing.py
@@ -8,20 +8,19 @@ from tgext.debugbar.sections.base import DebugSection
 from tgext.debugbar.utils import format_fname
 
 try:
-    import profile
+    import cPprofile as profile
 except ImportError:
-    profile = None
+    try:
+        import profile
+    except:
+        profile = None
 
 try:
     import pstats
 except ImportError:
     pstats = None
 
-def on_before_call(*args, **kw):
-    request.tgdb_call_start_time = time.time()
-
 def on_before_render(*args, **kw):
-    request.tgdb_call_time = (time.time() - request.tgdb_call_start_time)*1000
     request.tgdb_render_start_time = time.time()
 
 def on_after_render(*args, **kw):
@@ -29,60 +28,59 @@ def on_after_render(*args, **kw):
     request.tgdb_render_time = (now - request.tgdb_render_start_time)*1000
     request.tgdb_total_time = (now - request.tgdb_call_start_time)*1000
 
-lock = threading.Lock()
-if profile is not None:
-    profiler = profile.Profile()
-    
-def profile_wrapper(controller):
+def profile_wrapper(decoration, controller):
     def wrapped_controller(*args, **kw):
-        with lock:
-            try:
-                result = profiler.runcall(controller, *args, **kw)
-            except:
-                raise
-            finally:
-                stats = pstats.Stats(profiler)
-                function_calls = []
-                flist = stats.sort_stats('cumulative').fcn_list
-                for func in flist:
-                    current = {}
-                    info = stats.stats[func]
+        profiler = profile.Profile()
 
-                    # Number of calls
-                    if info[0] != info[1]:
-                        current['ncalls'] = '%d/%d' % (info[1], info[0])
-                    else:
-                        current['ncalls'] = info[1]
+        try:
+            request.tgdb_call_start_time = time.time()
+            result = profiler.runcall(controller, *args, **kw)
+            request.tgdb_call_time = (time.time() - request.tgdb_call_start_time)*1000
+        except:
+            raise
+        finally:
+            stats = pstats.Stats(profiler)
+            function_calls = []
+            flist = stats.sort_stats('cumulative').fcn_list
+            for func in flist:
+                current = {}
+                info = stats.stats[func]
 
-                    # Total time
-                    current['tottime'] = info[2] * 1000
+                # Number of calls
+                if info[0] != info[1]:
+                    current['ncalls'] = '%d/%d' % (info[1], info[0])
+                else:
+                    current['ncalls'] = info[1]
 
-                    # Quotient of total time divided by number of calls
-                    if info[1]:
-                        current['percall'] = info[2] * 1000 / info[1]
-                    else:
-                        current['percall'] = 0
+                # Total time
+                current['tottime'] = info[2] * 1000
 
-                    # Cumulative time
-                    current['cumtime'] = info[3] * 1000
+                # Quotient of total time divided by number of calls
+                if info[1]:
+                    current['percall'] = info[2] * 1000 / info[1]
+                else:
+                    current['percall'] = 0
 
-                    # Quotient of the cumulative time divded by the number
-                    # of primitive calls.
-                    if info[0]:
-                        current['percall_cum'] = info[3] * 1000 / info[0]
-                    else:
-                        current['percall_cum'] = 0
+                # Cumulative time
+                current['cumtime'] = info[3] * 1000
 
-                    # Filename
-                    filename = pstats.func_std_string(func)
-                    current['filename_long'] = filename
-                    current['filename'] = format_fname(filename)
-                    function_calls.append(current)
+                # Quotient of the cumulative time divded by the number
+                # of primitive calls.
+                if info[0]:
+                    current['percall_cum'] = info[3] * 1000 / info[0]
+                else:
+                    current['percall_cum'] = 0
 
-                request.tgdb_profiling_stats = stats
-                request.tgdb_profiling_function_calls = function_calls
+                # Filename
+                filename = pstats.func_std_string(func)
+                current['filename_long'] = filename
+                current['filename'] = format_fname(filename)
+                function_calls.append(current)
 
-            return result
+            request.tgdb_profiling_stats = stats
+            request.tgdb_profiling_function_calls = function_calls
+
+        return result
 
     return wrapped_controller
 
@@ -90,7 +88,6 @@ class TimingDebugSection(DebugSection):
     name = 'Timings'
     is_active = True
     hooks = dict(controller_wrapper=[profile_wrapper],
-                 before_call=[on_before_call],
                  before_render=[on_before_render],
                  after_render=[on_after_render])
 

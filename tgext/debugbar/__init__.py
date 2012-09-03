206118655e9041fe970db3c8260caf3ccffbed2f
Alessandro Molina <alessandro.molina@axant.it>
2012-09-03 12:56:45 +0200

    Avoid registering startup hooks while running inside a startup hook

--- tgext/debugbar/__init__.py
+++ tgext/debugbar/__init__.py
@@ -1,6 +1,13 @@
-from initialize import enable_debugbar
+from initialize import DebugBar, enable_debugbar
 
+try:
+    from tgext.pluggable.plug import ApplicationPlugger
+except ImportError:
+    ApplicationPlugger = None
 
 def plugme(app_config, options):
-    enable_debugbar(app_config)
+    if ApplicationPlugger:
+        DebugBar(app_config)()
+    else:
+        enable_debugbar(app_config)
     return dict(appid='tgext.debugbar')

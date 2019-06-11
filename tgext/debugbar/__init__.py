263f3b73190f427a9fad15cc81d6974227fac339
Alessandro Molina <amol@turbogears.org>
2019-06-11 22:00:28 +0200

    Add support for tgext.pluggable.plug on 2.4

--- tgext/debugbar/__init__.py
+++ tgext/debugbar/__init__.py
@@ -2,8 +2,16 @@ from .initialize import DebugBar, enable_debugbar
 
 
 def plugme(app_config, options):
-    app_config['debugbar.inventing'] = options.get('inventing', False)
-    app_config['debugbar.inventing_css'] = options.get('inventing_css', True)
-    app_config['debugbar.enable_logs'] = options.get('enable_logs', False)
+    try:
+        app_config.update_blueprint({
+            'debugbar.inventing': options.get('inventing', False),
+            'debugbar.inventing_css': options.get('inventing_css', True),
+            'debugbar.enable_logs': options.get('enable_logs', False)
+        })
+    except AttributeError:
+        app_config['debugbar.inventing'] = options.get('inventing', False)
+        app_config['debugbar.inventing_css'] = options.get('inventing_css', True)
+        app_config['debugbar.enable_logs'] = options.get('enable_logs', False)
+    
     enable_debugbar(app_config)
     return dict(appid='tgext.debugbar')
